b0b76c1533db4c9276f75ee3a1410135020f68dc
suige <suige787@live.jp>
2017-03-12 17:16:01 -0700

    :+1: change pattern images to my logo

--- app/bubbles.js
+++ app/bubbles.js
@@ -1,4 +1,5 @@
 (function() {
+  var logo = "M27.08,43c0.34-1.57,1.23-3.45,2.76-3.33a2.72,2.72,0,0,1,1.73,1.07C35,44.7,33.09,51,34.28,56.25a3.23,3.23,0,0,0,1,1.89,3.35,3.35,0,0,0,2.06.49L64.4,59a41.52,41.52,0,0,1,1.18-14.73,5.13,5.13,0,0,1,1-2.18c1.17-1.29,3.36-1,4.59.26A8,8,0,0,1,73,47.22a22.06,22.06,0,0,1-2.46,12.65,39.8,39.8,0,0,1,23,18.22,43.25,43.25,0,0,1,4.87,29.82A73.81,73.81,0,0,1,93,123.54c-1.26,2.8-2.63,5.66-4.93,7.6s-5.12,2.7-7.92,3.38c-19.66,4.79-40.77,5.78-59.63-1.76-3.88-1.55-7.74-3.53-10.56-6.73a27.53,27.53,0,0,1-4.56-7.92A64.26,64.26,0,0,1,.66,94.75c-0.08-6.77,1-13.81,4.7-19.36a43.65,43.65,0,0,1,6.32-7c3.84-3.69,7.93-7.55,13.08-9,1.64-.46,1.87,0,2.06-1.67,0.16-1.41-.48-3.31-0.56-4.76A37.41,37.41,0,0,1,27.08,43Z";
   var height = 600;
   var width = 1000;
 
@@ -44,16 +45,18 @@
 
   var defs = svg.append("defs");
   defs.append("pattern")
-    .attr("id", "js")
+    .attr("id", "logo")
     .attr("height", "100%")
     .attr("width", "100%")
     .attr("patternContentUnits", "objectBoundingBox")
-    .append("image")
+    .append("svg")
+    .attr("viewBox", "0 0 98.62 98.62")
     .attr("height", 1)
     .attr("width", 1)
     .attr("preserveAspectRation", "none")
-    .attr("xmls:xlink", "http://www.w3.org/1999/xlink")
-    .attr("xlink:href", "img/js.png");
+    .append("path")
+    .attr("d", logo)
+    .attr("transform", function(d) { return "translate(-0.66 -39.65)"; });
 
   var radiusScale = d3.scaleSqrt().domain([0, 100]).range([10, 60])
 
@@ -75,26 +78,6 @@
     .await(ready)
 
   function ready (error, datapoints) {
-
-    defs.selectAll(".skill-pattern")
-      .data(datapoints)
-      .enter().append("pattern")
-      .attr("class", "skill-pattern")
-      .attr("id", function(d){
-        return d.id;
-      })
-      .attr("height", "100%")
-      .attr("width", "100%")
-      .attr("patternContentUnits", "objectBoundingBox")
-      .append("image")
-      .attr("height", 1)
-      .attr("width", 1)
-      .attr("preserveAspectRation", "none")
-      .attr("xmls:xlink", "http://www.w3.org/1999/xlink")
-      .attr("xlink:href", function(d) {
-        return "img/" + d.image;
-      });
-
     var circles = svg.selectAll(".skill")
       .data(datapoints)
       .enter().append("circle")
@@ -103,7 +86,7 @@
         return radiusScale(d.parcent);
       })
       .attr("fill", function(d) {
-        return "url(#" + d.id + ")"
+        return "url(#logo)"
       })
       .on('click', function(d) {
         console.log(d);
